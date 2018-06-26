9309f2994900bb74041921f42f57cfd40f40f5b9
Dheeraj Mohan <dheeraj.coding@outlook.com>
2018-06-26 16:49:11 +0530

    Implement state for the slider.  Changes to be committed: 	modified:   index.js

--- src/index.js
+++ src/index.js
@@ -27,8 +27,7 @@ class Slider extends React.Component{
         this.handleWheel=this.handleWheel.bind(this);
     }
     
-    handleWheel(i){
-        return function (event){
+    handleWheel(i,event){
             event.preventDefault();
             event.stopPropagation();
             let rectObj=document.getElementById("slider").children[i].getBoundingClientRect();
@@ -37,7 +36,8 @@ class Slider extends React.Component{
                 if(dir){
                     if(scrollPoint<rectObj.height*(i+1)){
                         scrollPoint+=50;
-                        document.getElementById("slider").scrollTo(0,scrollPoint);    
+                        document.getElementById("slider").scrollTo(0,scrollPoint);
+                            
                     }else{
                         document.getElementById("slider").scrollTo(0,rectObj.height*(i+1));
                         clearInterval(timerID);
@@ -53,28 +53,65 @@ class Slider extends React.Component{
                 }
 
             }
-            if(event.deltaY>0){
+            if(event.deltaY>3){
                 let timerID=setInterval(()=>scroller(1,timerID),10);
             }
-            if(event.deltaY<0){
+            if(event.deltaY<-3){
                 let timerID=setInterval(()=>scroller(0,timerID),10);
             }
             
-        }
+        
     }
     
     render(){
         return(
             <ul id="slider">
-                <Menu name={this.menuList[0]} onWheel={this.handleWheel(0)} />
-                <Menu name={this.menuList[1]} onWheel={this.handleWheel(1)} />
-                <Menu name={this.menuList[2]} onWheel={this.handleWheel(2)} />
-                <Menu name={this.menuList[3]} onWheel={this.handleWheel(3)} />
+                <Menu name={this.menuList[0]} onWheel={
+                    (function(event){
+                        this.handleWheel(0,event);
+                        this.props.update(0);
+                        return false;
+                    }).bind(this) } />
+                <Menu name={this.menuList[1]} onWheel={
+                    (function(event){
+                        this.handleWheel(1,event);
+                        this.props.update(1);
+                        return false;
+                    }).bind(this) } />
+                <Menu name={this.menuList[2]} onWheel={
+                    (function(event){
+                        this.handleWheel(2,event);
+                        this.props.update(2);
+                        return false;
+                    }).bind(this) } />
+                <Menu name={this.menuList[3]} onWheel={
+                    (function(event){
+                        this.handleWheel(3,event);
+                        this.props.update(3);
+                        return false;
+                    }).bind(this) } />
             </ul>
         );
     }
 }
-
+class Box extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={active:0};
+        this.updateState=this.updateState.bind(this);
+    }
+    updateState(i){
+        this.setState({active:i});
+    }
+    render(){
+        return(
+            <div className="box">
+                <Slider update={this.updateState}/>
+            </div>
+            
+        );
+    }
+}
 
 class App extends React.Component{
     constructor(props){
@@ -85,9 +122,7 @@ class App extends React.Component{
             <div>
                 <Header/>
                 <div className="container">
-                    <div className="box">
-                        <Slider />
-                    </div>
+                    <Box/>
                 </div>
             </div>
         );
