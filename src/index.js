45b8dab71bdb70ccbd6bb5c1327abcbb703a73ec
Dheeraj Mohan <dheeraj.coding@outlook.com>
2018-06-26 21:01:43 +0530

    Implement keypress for navigation in slider  Changes to be committed: 	modified:   dist/index.html 	deleted:    dist/particles.js 	modified:   package-lock.json 	modified:   package.json 	modified:   src/index.js 	modified:   src/style.scss

--- src/index.js
+++ src/index.js
@@ -4,6 +4,19 @@ import './style.scss';
 import Profile from './profile.jpeg';
 import X from 'react-feather/dist/icons/x';
 import Circle from 'react-feather/dist/icons/circle';
+import Particles from 'reactparticles.js';
+function generateMenu(i){
+    return(<Menu name={this.menuList[i]} 
+        onWheel={(function(event){
+                    this.handleWheel(i,event);
+                    if(event.deltaY>3){
+                        this.props.update(i,1);
+                    }else if(event.deltaY<-3){
+                        this.props.update(i,0);
+                    }
+                    return false;}).bind(this) } 
+            />);
+}
 class Header extends React.Component{
     render(){
         return(
@@ -17,8 +30,8 @@ class Header extends React.Component{
 class Menu extends React.Component{
     render(){
         return(
-            <li onWheel={this.props.onWheel} ><div>{this.props.name}</div></li>
-        );
+            <li onWheel={this.props.onWheel}><div>{this.props.name}</div></li>
+        );1
     }
 }
 class Indicator extends React.Component{
@@ -44,6 +57,7 @@ class Slider extends React.Component{
         super(props);
         this.menuList=['Home','Works','About','Resume'];
         this.handleWheel=this.handleWheel.bind(this);
+        generateMenu=generateMenu.bind(this);
     }
     
     handleWheel(i,event){
@@ -77,57 +91,66 @@ class Slider extends React.Component{
             }
             if(event.deltaY<-3){
                 let timerID=setInterval(()=>scroller(0,timerID),10);
-            }
-            
+            }          
         
     }
+
+    handleKeyPress(i,event){
+        event.preventDefault();
+        event.stopPropagation();
+        let rectObj=document.getElementById("slider").children[i].getBoundingClientRect();
+        let scrollPoint=rectObj.height*i;
+        function scroller(dir,timerID){
+            if(dir){
+                if(scrollPoint<rectObj.height*(i+1)){
+                    scrollPoint+=50;
+                    document.getElementById("slider").scrollTo(0,scrollPoint);
+                        
+                }else{
+                    document.getElementById("slider").scrollTo(0,rectObj.height*(i+1));
+                    clearInterval(timerID);
+                }
+            }else{
+                if(scrollPoint>rectObj.height*(i-1)){
+                    scrollPoint-=50;
+                    document.getElementById("slider").scrollTo(0,scrollPoint);    
+                }else{
+                    document.getElementById("slider").scrollTo(0,rectObj.height*(i-1));
+                    clearInterval(timerID);
+                }
+            }
+
+        }
+        if(event.keyCode===40){
+            let timerID=setInterval(()=>scroller(1,timerID),10);
+        }
+        if(event.keyCode===38){
+            let timerID=setInterval(()=>scroller(0,timerID),10);
+        }          
     
+    }
+
+    componentDidMount(){
+        document.addEventListener("keypress",
+            (function(event){
+                this.handleKeyPress(this.props.focus,event);
+                if(event.keyCode==40){
+                    this.props.update(this.props.focus,1);
+                }else if(event.keyCode===38){
+                    this.props.update(this.props.focus,0);
+                }
+            }).bind(this)
+        );
+    }
+
     render(){
         return(
             <ul id="slider">
-                <Menu name={this.menuList[0]} onWheel={
-                    (function(event){
-                        this.handleWheel(0,event);
-                        if(event.deltaY>3){
-                            this.props.update(0,1);
-                        }else if(event.deltaY<-3){
-                            this.props.update(0,0);
-                        }
-                        
-                        return false;
-                    }).bind(this) } />
-                <Menu name={this.menuList[1]} onWheel={
-                    (function(event){
-                        this.handleWheel(1,event);
-                        if(event.deltaY>3){
-                            this.props.update(1,1);
-                        }else if(event.deltaY<-3){
-                            this.props.update(1,0);
-                        }
-                        return false;
-                    }).bind(this) } />
-                <Menu name={this.menuList[2]} onWheel={
-                    (function(event){
-                        this.handleWheel(2,event);
-                        if(event.deltaY>3){
-                            this.props.update(2,1);
-                        }else if(event.deltaY<-3){
-                            this.props.update(2,0);
-                        }
-                        return false;
-                    }).bind(this) } />
-                <Menu name={this.menuList[3]} onWheel={
-                    (function(event){
-                        this.handleWheel(3,event);
-                        if(event.deltaY>3){
-                            this.props.update(3,1);
-                        }else if(event.deltaY<-3){
-                            this.props.update(3,0);
-                        }
-                        return false;
-                    }).bind(this) } />
-
-                    <Indicator focus={this.props.focus}/>
+                {generateMenu(0)}
+                {generateMenu(1)}
+                {generateMenu(2)}
+                {generateMenu(3)}
+                <Indicator focus={this.props.focus}/>
             </ul>
         );
     }
@@ -140,7 +163,6 @@ class Box extends React.Component{
     }
     updateState(i,dir){
         this.setState({active:(dir?(i===3?i:i+1):(i===0?i:i-1))});
-        console.log(i);
     }
     render(){
         return(
@@ -161,7 +183,8 @@ class App extends React.Component{
             <div>
                 <Header/>
                 <div className="container" id="particles-js">
-                    <Box/>
+                    <Particles id="particles" config="particlesjs-config.json"/>
+                    <Box/>                    
                 </div>
             </div>
         );
