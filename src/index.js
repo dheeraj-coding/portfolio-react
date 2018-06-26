5a95914a8b09c31ed5ccdb236f16f627d5ef91ad
Dheeraj Mohan <dheeraj.coding@outlook.com>
2018-06-26 14:12:57 +0530

    Finish Scroll feature of the slider 	new file:   .vscode/launch.json 	modified:   src/index.js 	modified:   src/style.scss

--- src/index.js
+++ src/index.js
@@ -12,18 +12,62 @@ class Header extends React.Component{
         );
     }
 }
-class Container extends React.Component{
+class Menu extends React.Component{
     render(){
         return(
-            <div class="container">
-                <div class="box">
-
-                </div>
-
-            </div>
+            <li onWheel={this.props.onWheel}><div>{this.props.name}</div></li>
         );
     }
 }
+
+class Slider extends React.Component{
+    constructor(props){
+        super(props);
+        this.menuList=['Home','Works','About','Resume'];
+        this.handleWheel=this.handleWheel.bind(this);
+        this.handleKeyPress=this.handleKeyPress.bind(this);
+    }
+    
+    handleWheel(i){
+        return function (event){
+            let rect=document.getElementById("slider").children[i].getBoundingClientRect();
+            if(event.deltaY>0){
+                document.getElementById("slider").scrollTo(0,rect.height*(i+1));
+            }
+            if(event.deltaY<0){
+                document.getElementById("slider").scrollTo(0,rect.height*(i-1));
+            }
+            
+        }
+    }
+
+    handleKeyPress(i){
+        return function (event){
+            console.log(event.key);
+            let rect=document.getElementById("slider").children[i].getBoundingClientRect();
+            if(event.keyCode===38){
+                document.getElementById("slider").scrollTo(0,rect.height*(i+1));
+            }
+            if(event.keyCode===40){
+                document.getElementById("slider").scrollTo(0,rect.height*(i-1));
+            }
+            
+        }
+    }
+    
+    render(){
+        return(
+            <ul id="slider">
+                <Menu name={this.menuList[0]} onWheel={this.handleWheel(0)} />
+                <Menu name={this.menuList[1]} onWheel={this.handleWheel(1)} />
+                <Menu name={this.menuList[2]} onWheel={this.handleWheel(2)} />
+                <Menu name={this.menuList[3]} onWheel={this.handleWheel(3)} />
+            </ul>
+        );
+    }
+}
+
+
 class App extends React.Component{
     constructor(props){
         super(props);
@@ -32,7 +76,11 @@ class App extends React.Component{
         return (
             <div>
                 <Header/>
-                <Container/>
+                <div className="container">
+                    <div className="box">
+                        <Slider />
+                    </div>
+                </div>
             </div>
         );
     }
